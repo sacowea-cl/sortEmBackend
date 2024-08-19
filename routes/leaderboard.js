2b1d33df7172ea8fa46695630899334b900498b9
Bruno Farfán <bruno.farfan.miquel@uc.cl>
2024-08-19 00:41:05 -0400

    delete password

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Leaderboard = require('../models/leaderboard');
+require('dotenv').config();
 
 // Get the leaderboard
 router.get('/leaderboard', async (req, res) => {
@@ -23,6 +24,24 @@ router.post('/leaderboard', async (req, res) => {
         const { username, moves, time } = req.body;
         const newEntry = await Leaderboard.create({ username, moves, time });
         res.json(newEntry);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// Delete all scores
+router.patch('/leaderboard/pablochile', async (req, res) => {
+    try {
+        const { password } = req.body;
+        if (password !== process.env.DELETE_PASSWORD) {
+            return res.status(401).json({ msg: 'Unauthorized' });
+        }
+        await Leaderboard.destroy({
+            where: {},
+            truncate: true
+        });
+        res.json({ msg: 'All scores deleted' });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
